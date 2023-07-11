28434cb0f78b4a16fdc6cf6f5e299773ca5dbb95
znxaz <zsazzam@ndu.edu.lb>
2023-07-11 14:44:44 +0300

    server update

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors') ; 
 const app = express();
-const port = 3001;
+const port = 3000;
 
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require('./routes/userRouts');
 
+app.use(cors())
 app.use(express.json());
 
 app.use('/users/api', userRoutes);
